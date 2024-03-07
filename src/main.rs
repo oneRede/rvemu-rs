841035072a4db6c7d49415642fa10e14784fd751
rede <rede@rededeMacBook-Pro.local>
2024-03-07 11:09:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod elfdef;
-mod rvemu;
-mod reg;
 fn main() {
-    let d8: [u8;8] = [0,0,0,0,0,0,0,1];
-    println!("{:?}", d8);
-    let ptr8 = &d8 as *const u8;
-    let ptr64 = ptr8 as *const u64;
-    let dd = unsafe { *ptr64 };
-    println!("{:?}", dd);
+    println!("Hello World!!")
 }
